4dbf8e9921aea6a089c9b80237bc2a4bf6ceebf3
dkthemonkey <donggui7@gmail.com>
2020-06-02 22:20:26 +0900

    fix - dummy text

--- src/app.ts
+++ src/app.ts
@@ -2,30 +2,18 @@ import express, { Application, Request, Response, NextFunction } from "express";
 
 const app: Application = express();
 
-const add = (a: number, b: number): number => a + b;
-
 app.get("/", (req: Request, res: Response, next: NextFunction) => {
-  console.log(add(3, 4));
-
   res.send(`
   <!DOCTYPE html>
   <html lang="ja">
    <head>
     <meta charset="UTF-8">
-    <link rel="author" href="mailto:mail@example.com">
-    <title lang="en">HyperText Markup Language - Wikipedia</title>
+    <title>Express TypeScript</title>
    </head>
    <body>
     <article>
-     <h1 lang="en">HyperText Markup Language</h1>
-     <p>HTMLは、<a href="http://ja.wikipedia.org/wiki/SGML">SGML</a>
-        アプリケーションの一つで、ハイパーテキストを利用してワールド
-        ワイドウェブ上で情報を発信するために作られ、
-        ワールドワイドウェブの<strong>基幹的役割</strong>をなしている。
-        情報を発信するための文書構造を定義するために使われ、
-        ある程度機械が理解可能な言語で、
-        写真の埋め込みや、フォームの作成、
-        ハイパーテキストによるHTML間の連携が可能である。</p>
+     <h1>Dummy Text</h1>
+     <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
     </article>
    </body>
   </html>
